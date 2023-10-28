ee90266830ff0c9eec58659249461c5b6da847ab
Minwoo Kim <minwook1130@gmail.com>
2023-10-27 20:14:09 -0400

    Created DragBlockAnimation script

---
+++ Assets/Scripts/DragBlockAnimation.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragBlockAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private AnimationCurve animationCurve;
+    [SerializeField]
+    private float appearingTime;
+
+    public void PlayAnimation(Vector3 spawningPoint)
+    {
+        StartCoroutine(MoveTo(spawningPoint, appearingTime));
+    }
+
+    private IEnumerator MoveTo(Vector3 end, float moveTime)
+    {
+        Vector3 start = transform.position;
+        float current = 0;
+        float percent = 0;
+
+        while ( percent < 1 )
+        {
+            current += Time.deltaTime;
+            percent = current / moveTime;
+
+            transform.position = Vector3.Lerp(start, end, animationCurve.Evaluate(percent));
+
+            yield return null;
+        }
+    }
+}
