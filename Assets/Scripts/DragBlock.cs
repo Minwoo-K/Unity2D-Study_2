e113217177b5cb1869e1692df2a297129622166a
Minwoo Kim <minwook1130@gmail.com>
2023-11-01 15:27:14 -0400

    Updated DragBlock script
    - Block counts property
    - OnMouse Down/Drag/Up function definitions
    all in progress

--- Assets/Scripts/DragBlock.cs
+++ Assets/Scripts/DragBlock.cs
@@ -7,11 +7,33 @@ public class DragBlock : MonoBehaviour
     [SerializeField]
     private AnimationCurve animationCurve;
     [SerializeField]
+    private AnimationCurve scaleCurve;
+    [SerializeField]
     private float appearingTime;
+    [field:SerializeField]
+    public Vector2Int BlockCounts { get; private set; }
+
+    private Vector3 offset = new Vector3(0, 0, 10);      // offset value of mouse position when being dragged
 
     public void Initialized(Vector3 spawningPoint)
     {
         StartCoroutine(MoveTo(spawningPoint, appearingTime));
+    }
+
+    private void OnMouseDown()
+    {
+        transform.localScale = Vector3.one;
+    }
+
+    private void OnMouseDrag()
+    {
+        
+    }
+
+    private void OnMouseUp()
+    {
+        transform.localScale = Vector3.one * 0.5f;
+        transform.position = transform.parent.position;
     }
 
     private IEnumerator MoveTo(Vector3 end, float moveTime)
