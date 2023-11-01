abb195dc2962b5fd39688de75fac7409f576cf97
Minwoo Kim <minwook1130@gmail.com>
2023-11-01 15:49:43 -0400

    Added Scaling & Moving Coroutines for DragBlock

--- Assets/Scripts/DragBlock.cs
+++ Assets/Scripts/DragBlock.cs
@@ -13,27 +13,31 @@ public class DragBlock : MonoBehaviour
     [field:SerializeField]
     public Vector2Int BlockCounts { get; private set; }
 
-    private Vector3 offset = new Vector3(0, 0, 10);      // offset value of mouse position when being dragged
+    private Vector3 offset;     // offset value of mouse position when being dragged
+    private float returningTime = 0.1f;
 
     public void Initialized(Vector3 spawningPoint)
     {
+        offset = new Vector3(0, BlockCounts.y * 0.5f, 10);
         StartCoroutine(MoveTo(spawningPoint, appearingTime));
     }
 
     private void OnMouseDown()
     {
-        transform.localScale = Vector3.one;
+        StopCoroutine("ScaleTo");
+        StartCoroutine(ScaleTo(Vector3.one));
     }
 
     private void OnMouseDrag()
     {
-        
+        Vector3 input = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = input + offset;
     }
 
     private void OnMouseUp()
     {
-        transform.localScale = Vector3.one * 0.5f;
-        transform.position = transform.parent.position;
+        StartCoroutine(ScaleTo(Vector3.one * 0.5f));
+        StartCoroutine(MoveTo(transform.parent.position, returningTime));
     }
 
     private IEnumerator MoveTo(Vector3 end, float moveTime)
@@ -48,6 +52,23 @@ public class DragBlock : MonoBehaviour
             percent = current / moveTime;
 
             transform.position = Vector3.Lerp(start, end, animationCurve.Evaluate(percent));
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator ScaleTo(Vector3 end)
+    {
+        Vector3 start = transform.localScale;
+        float current = 0;
+        float percent = 0;
+
+        while ( percent < 1 )
+        {
+            current += Time.deltaTime;
+            percent = current / returningTime;
+
+            transform.localScale = Vector3.Lerp(start, end, scaleCurve.Evaluate(percent));
 
             yield return null;
         }
