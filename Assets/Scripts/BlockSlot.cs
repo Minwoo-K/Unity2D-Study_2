b878b252b382ac0aca171820f0d24b47a46d4020
Minwoo Kim <minwook1130@gmail.com>
2023-11-10 16:07:23 -0500

    Added BlockSlot Emptying animation

--- Assets/Scripts/BlockSlot.cs
+++ Assets/Scripts/BlockSlot.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class BlockSlot : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
-    
+    private float scalingTime = 0.15f;
+
     public bool IsFilled { get; private set; } = false;
 
     private void Awake()
@@ -21,7 +22,27 @@ public class BlockSlot : MonoBehaviour
 
     public void GetEmpty()
     {
-        IsFilled = false;
+        StartCoroutine(EmptyingAnimation(Vector3.zero));
+    }
+
+    private IEnumerator EmptyingAnimation(Vector3 end)
+    {
+        Vector3 start = transform.localScale;
+        float current = 0;
+        float percent = 0;
+
+        while ( percent < 1 )
+        {
+            current += Time.deltaTime;
+            percent = current / scalingTime;
+
+            transform.localScale = Vector3.Lerp(start, end, percent);
+
+            yield return null;
+        }
+
         spriteRenderer.color = Color.white;
+        transform.localScale = Vector3.one;
+        IsFilled = false;
     }
 }
