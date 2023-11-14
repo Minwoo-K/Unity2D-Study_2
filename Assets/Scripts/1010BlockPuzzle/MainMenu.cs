610cf24d4a6fbeb20cd6602bc776a95589dd2dcb
Minwoo Kim <minwook1130@gmail.com>
2023-11-14 15:50:43 -0500

    Created MainMenu Script

---
+++ Assets/Scripts/1010BlockPuzzle/MainMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI highestScoreText;
+
+    public void ButtonGameStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ButtonExit()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit();
+        #endif
+    }
+}
