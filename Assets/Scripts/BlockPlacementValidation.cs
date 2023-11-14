05ee26b639f0da6f99956bc96a59a239a544d8fe
Minwoo Kim <minwook1130@gmail.com>
2023-11-13 20:55:07 -0500

    Added Block Spot Check function
    - To check whether a given DragBlock can be placed in the current board

--- Assets/Scripts/BlockPlacementValidation.cs
+++ Assets/Scripts/BlockPlacementValidation.cs
@@ -63,6 +63,43 @@ public class BlockPlacementValidation : MonoBehaviour
         return false;
     }
 
+    public bool IsPossibleToPlaceBlock(DragBlock dragBlock)
+    {
+
+        for ( int y = 0; y < blockCount.y; y++ )
+        {
+            for ( int x = 0; x < blockCount.x; x++ )
+            {
+                // To count how many blocks can be put at a time within the DragBlock
+                int count = 0;
+                // Figure out the position to check
+                Vector3 position = new Vector3(-blockCount.x / 2f + blockHalf.x + x, blockCount.y / 2f - blockHalf.y - y, 0);
+                // if even count blocks(x==2 or y==2), neat number without decimal point([-4,4], [-3,3] ...)
+                // if odd count blocks (x==1 or y==3), a number with decimal point([-4.5, 4.5], [-3.5, 4.5] ...) to check available spots
+                position.x = dragBlock.blockCount.x % 2 == 0 ? dragBlock.blockCount.x + 0.5f : dragBlock.blockCount.x;
+                position.y = dragBlock.blockCount.y % 2 == 0 ? dragBlock.blockCount.y - 0.5f : dragBlock.blockCount.y;
+
+                // Check each block can be placed. If any one of them isn't, break the loop to go to the next spot
+                for ( int i = 0; i < dragBlock.ChildBlockPositions.Length; i++ )
+                {
+                    Vector3 oneBlockPosition = position + dragBlock.ChildBlockPositions[i];
+
+                    if (IsBlockOutsideTheMap(oneBlockPosition)) break;
+
+                    if (IsOtherBlockOnSpot(oneBlockPosition)) break;
+
+                    count++;
+                }
+
+                // If count is equal to the number of blocks the DragBlock has, that means it can be placed within the board
+                if ( count == dragBlock.ChildBlockPositions.Length ) return true;
+            }
+        }
+
+        Debug.Log("Game Over");
+        return false;
+    }
+
     private int GetIndexFromPosition(Vector3 position)
     {
         float x = blockCount.x / 2f - blockHalf.x + position.x;
