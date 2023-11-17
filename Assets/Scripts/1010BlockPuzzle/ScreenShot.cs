c08590e7b4a0a78427aeab59bd09270d09d4cc55
Minwoo Kim <minwook1130@gmail.com>
2023-11-17 14:54:59 -0500

    Writing ScreenShot class in progress

--- Assets/Scripts/1010BlockPuzzle/ScreenShot.cs
+++ Assets/Scripts/1010BlockPuzzle/ScreenShot.cs
@@ -4,5 +4,33 @@ using UnityEngine;
 
 public class ScreenShot : MonoBehaviour
 {
+    [SerializeField]
+    private Camera screenshotCamera;
 
+    public Sprite ScreenshotToSprit()
+    {
+        // [Screen] == Game Screen
+        int width = Screen.width;
+        int height = Screen.height;
+
+        // Create a Texture that Screenshot goes into with the given width and height
+        RenderTexture screenshotContainer = new RenderTexture(width, height, 24);
+        // Register the screenshotTexture into the Camera's targetTexture variable where the Camera renders
+        screenshotCamera.targetTexture = screenshotContainer;
+
+        // Render the Camera
+        screenshotCamera.Render();
+        RenderTexture.active = screenshotContainer;
+
+        // Read the Pixels information from the active RenderTexture
+        // Save the Texture2D into the [screenshot]
+        Texture2D screenshot = new Texture2D(width, height, TextureFormat.RGB24, false);
+        screenshot.ReadPixels(new Rect(0, (height - width) * 0.5f, width, width), 0, 0);
+        screenshot.Apply();
+
+        // Empty the targetTexture as Screenshotting is done
+        screenshotCamera.targetTexture = null;
+
+        return null;
+    }
 }
