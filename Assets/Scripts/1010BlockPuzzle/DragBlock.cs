5fe4d88dd33893f9e8be6d37a5bf877ec2413e58
Minwoo Kim <minwook1130@gmail.com>
2023-11-20 15:34:03 -0500

    Created DragBlock script

--- Assets/Scripts/1010BlockPuzzle/DragBlock.cs
+++ Assets/Scripts/1010BlockPuzzle/DragBlock.cs
@@ -6,4 +6,22 @@ public class DragBlock : MonoBehaviour
 {
     [SerializeField]
     private Vector2Int blockNumber;
+
+    // When clicked to be moved
+    private void OnMouseDown()
+    {
+        
+    }
+
+    // When being moved
+    private void OnMouseDrag()
+    {
+        
+    }
+
+    // When dropped
+    private void OnMouseUp()
+    {
+        
+    }
 }
