3ce29674cd917ebaf3ab23c5da98eebca33b190a
Minwoo Kim <minwook1130@gmail.com>
2023-11-20 15:16:04 -0500

    Created DragBlock script
    - Set up DragBlock prefabs with Block Composition Numbers

---
+++ Assets/Scripts/1010BlockPuzzle/DragBlock.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragBlock : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2Int blockNumber;
+}
