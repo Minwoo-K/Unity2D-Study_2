b65096c8aa8470e6c1055bf2e3d37e1fd38845ba
Minwoo Kim <minwook1130@gmail.com>
2023-11-21 14:01:15 -0500

    Implemented BlockSlot script

--- Assets/Scripts/1010BlockPuzzle/BlockSlot.cs
+++ Assets/Scripts/1010BlockPuzzle/BlockSlot.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class BlockSlot : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
 
+    public bool IsFilled { get; private set; } = false;
+
+    public void Initialized()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void GetFilled(Color color)
+    {
+        IsFilled = true;
+
+        spriteRenderer.color = color;
+    }
+
+    public void GetEmpty()
+    {
+        IsFilled = false;
+
+        spriteRenderer.color = Color.white;
+    }    
 }
