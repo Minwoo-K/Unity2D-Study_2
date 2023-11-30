d30af771b586fe5160144bdc10ab8d6f0266e597
Minwoo Kim <minwook1130@gmail.com>
2023-11-29 21:21:10 -0500

    Need Version update, and in progress

--- Assets/Scripts/2048/Board.cs
+++ Assets/Scripts/2048/Board.cs
@@ -23,6 +23,7 @@ public class Board : MonoBehaviour
 
     private void Start()
     {
+        // Need a version update to use this feature
         UnityEngine.UI.LayoutRebuilder.ForceRebuildLayoutImmediate(nodeSpawner.GetComponent<RectTransform>());
 
         foreach ( Node node in NodeList )
