34e6a610f65ee8e7d9d2372d0337790f37655c24
Minwoo Kim <minwook1130@gmail.com>
2023-11-29 21:07:19 -0500

    Implemented Board script

--- Assets/Scripts/2048/Board.cs
+++ Assets/Scripts/2048/Board.cs
@@ -25,6 +25,49 @@ public class Board : MonoBehaviour
     {
         UnityEngine.UI.LayoutRebuilder.ForceRebuildLayoutImmediate(nodeSpawner.GetComponent<RectTransform>());
 
+        foreach ( Node node in NodeList )
+        {
+            node.localPosition = node.GetComponent<RectTransform>().localPosition;
+        }
 
+        SpawnBlockAtRandomNode();
+        SpawnBlockAtRandomNode();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("1")) SpawnBlockAtRandomNode();
+    }
+
+    private void SpawnBlockAtRandomNode()
+    {
+        List<Node> emptyNodes = NodeList.FindAll(node => node.blockInfo == null);
+
+        if ( emptyNodes.Count != 0 )
+        {
+            int index = Random.Range(0, emptyNodes.Count);
+            Vector2Int coor = emptyNodes[index].Coordinate;
+
+            SpawnBlock(coor.x, coor.y);
+        }
+        else
+        {
+            // Check whether or not Game Over
+        }
+    }
+
+    private void SpawnBlock(int x, int y)
+    {
+        if ( NodeList[y * BlockCount.x + x].blockInfo != null ) return;
+
+        GameObject clone = Instantiate(blockPrefab, blockParent);
+
+        Block block = clone.GetComponent<Block>();
+
+        Node node = NodeList[y * BlockCount.x + x];
+
+        clone.GetComponent<RectTransform>().localPosition = node.localPosition;
+
+        node.blockInfo = block;
     }
 }
