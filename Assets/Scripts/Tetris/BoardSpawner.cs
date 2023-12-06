ca913c71fa75058117342e12b4e155a5c81f4848
Minwoo Kim <minwook1130@gmail.com>
2023-12-06 17:03:50 -0500

    Set up the main camera's size and position

---
+++ Assets/Scripts/Tetris/BoardSpawner.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tetris
+{
+    public class BoardSpawner : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject blockPrefab;
+    }
+}
