c3d1c920221dbdd73272dacf23665e58b13f7c2a
Minwoo Kim <minwook1130@gmail.com>
2023-12-07 14:14:07 -0500

    Adjusted spawning points & updated the Spawner

--- Assets/Scripts/Tetris/TetrisBlockSpawner.cs
+++ Assets/Scripts/Tetris/TetrisBlockSpawner.cs
@@ -13,11 +13,17 @@ public class TetrisBlockSpawner : MonoBehaviour
 
     private void Awake()
     {
-        
+        for ( int i = 0; i < spawningPoints.Length; i++ )
+        {
+            SpawnTetrisBlock(spawningPoints[i]);
+        }
     }
 
-    public void SpawnTetrisBlock()
+    public void SpawnTetrisBlock(Transform spawningPoint)
     {
+        // Generate a random number to randomly spawn a TetrisBlock
         int random = Random.Range(0, tetrisBlockPrefabs.Length);
+        // Spawn a TetrisBlock with the random number
+        GameObject clone = Instantiate(tetrisBlockPrefabs[random], spawningPoint);
     }
 }
