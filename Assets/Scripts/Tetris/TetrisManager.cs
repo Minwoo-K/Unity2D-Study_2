3776ba69035d9e97b1f2da14c28bf282150168bd
Minwoo Kim <minwook1130@gmail.com>
2023-12-11 15:35:33 -0500

    Changed NextBlocks array to List

--- Assets/Scripts/Tetris/TetrisManager.cs
+++ Assets/Scripts/Tetris/TetrisManager.cs
@@ -6,19 +6,34 @@ public class TetrisManager : MonoBehaviour
 {
     [Header("Core Objects")]
     [SerializeField]
-    private Transform[] blockSpawningPoints;
+    private Transform[] blockSpawningPoints;        // Random Spawning Points in the board
     [SerializeField]
-    private TetrisBlockSpawner tetrisBlockSpawner;
+    private TetrisBlockSpawner tetrisBlockSpawner;  // TetrisBlockSpawner Component
 
-    private TetrisBlock[] nextBlocks;
-    private readonly int tetrisBlockCount = 3;
+    private List<TetrisBlock> nextBlocks;               // Next Blocks on the right panel
+    private readonly int tetrisBlockCount = 3;      // Maximum Count of Next Blocks standing by
 
+    // Game Start
     private void Start()
     {
-        nextBlocks = new TetrisBlock[tetrisBlockCount];
+        nextBlocks = new List<TetrisBlock>();
         for ( int i = 0; i < tetrisBlockCount; i++ )
         {
-            nextBlocks[i] = tetrisBlockSpawner.SpawnTetrisBlock(tetrisBlockSpawner.SpawningPoints[i]);
+            nextBlocks.Add(tetrisBlockSpawner.SpawnTetrisBlock(tetrisBlockSpawner.SpawningPoints[i]));
         }
+
+        PlaceNextTetrisBlock();
+    }
+
+    private void PlaceNextTetrisBlock()
+    {
+        // Get a random number from the spawning points in the board
+        int random = Random.Range(0, blockSpawningPoints.Length);
+        // 
+        nextBlocks[0].transform.SetParent(blockSpawningPoints[random], false);
+        //
+        nextBlocks.RemoveAt(0);
+        //
+        
     }
 }
