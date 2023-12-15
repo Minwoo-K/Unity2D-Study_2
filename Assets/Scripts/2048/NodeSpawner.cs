ec6463cad5ec7c8551feff351ae613775d2ddb7c
Minwoo Kim <minwook1130@gmail.com>
2023-12-15 15:13:50 -0500

    Updated NodeSpawner script

--- Assets/Scripts/2048/NodeSpawner.cs
+++ Assets/Scripts/2048/NodeSpawner.cs
@@ -9,16 +9,26 @@ public class NodeSpawner : MonoBehaviour
     [SerializeField]
     private RectTransform nodeParent;
 
-    private void Awake()
+    public List<Node> SpawnNodes(Vector2Int blockCount)
     {
-        Vector2Int blockCount = new Vector2Int(4, 4);
+        List<Node> nodeList = new List<Node>();
 
         for ( int y = 0; y < blockCount.y; y++ )
         {
             for ( int x = 0; x < blockCount.x; x++ )
             {
+                // Spawn a Node of the prefab
                 GameObject clone = Instantiate(nodePrefab, nodeParent);
+                // Name the Node object as its coordinate
+                clone.name = $"Node[ {x}, {y} ]";
+                // Fetch Node component and initialize it
+                Node node = clone.GetComponent<Node>();
+                node.Initialized(new Vector2Int(x, y));
+                // Add the Node to the List
+                nodeList.Add(node);
             }
         }
+        // return the List of Nodes
+        return nodeList;
     }
 }
