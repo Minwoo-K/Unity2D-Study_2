7cd7e64d0004789f915e4c7814a11c398e85ef1d
Minwoo Kim <minwook1130@gmail.com>
2023-12-15 15:01:38 -0500

    Created Node script

---
+++ Assets/Scripts/2048/Node.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Block blockInfo;
+    public Vector2 localPosition;
+
+    public Vector2Int Coordinate { get; private set; }
+
+    public void Initialized(Vector2Int coordinate)
+    {
+        Coordinate = coordinate;
+    }
+}
