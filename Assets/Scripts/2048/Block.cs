733740d448f71ca5992de020e6f6d94327a63e85
Minwoo Kim <minwook1130@gmail.com>
2023-12-15 16:05:40 -0500

    Creeated Board and Updating Board script in progress

--- Assets/Scripts/2048/Block.cs
+++ Assets/Scripts/2048/Block.cs
@@ -4,25 +4,5 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    [SerializeField]
-    private NodeSpawner nodeSpawner;
-    [SerializeField]
-    private GameObject blockPrefab;
-
-    public List<Node> NodeList { get; private set; }
-    public Vector2Int BlockCount { get; private set; }
-
-    private void Awake()
-    {
-        BlockCount = new Vector2Int(4, 4);
-
-        NodeList = new List<Node>(); 
-        NodeList = nodeSpawner.SpawnNodes(BlockCount);
-    }
-    private void Start()
-    {
-        UnityEngine.UI.LayoutRebuilder.ForceRebuildLayoutImmediate(nodeSpawner.GetComponent<RectTransform>());
-
-
-    }
+    
 }
---
+++ Assets/Scripts/2048/Board.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [SerializeField]
+    private NodeSpawner nodeSpawner;
+    [SerializeField]
+    private GameObject blockPrefab;
+
+    public List<Node> NodeList { get; private set; }
+    public Vector2Int BlockCount { get; private set; }
+
+    private void Awake()
+    {
+        BlockCount = new Vector2Int(4, 4);
+
+        NodeList = new List<Node>();
+        NodeList = nodeSpawner.SpawnNodes(BlockCount);
+    }
+    private void Start()
+    {
+        UnityEngine.UI.LayoutRebuilder.ForceRebuildLayoutImmediate(nodeSpawner.GetComponent<RectTransform>());
+
+
+    }
+
+    private void SpawnBlockAtRandomNode()
+    {
+        List<Node> emptyNodes = NodeList.FindAll(node => node.blockInfo == null);
+
+        if (emptyNodes.Count != 0)
+        {
+            int index = Random.Range(0, emptyNodes.Count);
+            // To-Do: Figure out a coordinate of the Node to spawn a block
+        }
+        else
+        {
+            // Game Over check
+        }
+    }
+
+    // To-Do: Write a function that spawns a block based on a coordinate value
+}
