2d9bcc4cec8c0e11cf84c724f9bdd7882ab3b671
Minwoo Kim <minwook1130@gmail.com>
2023-12-18 16:13:52 -0500

    Implemented Block script

--- Assets/Scripts/2048/Block.cs
+++ Assets/Scripts/2048/Block.cs
@@ -1,8 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Block : MonoBehaviour
 {
-    
+    [SerializeField]
+    private Color[] blockColorList;
+    [SerializeField]
+    private Image blockImage;
+    [SerializeField]
+    private TextMeshProUGUI textBlockNumber;
+
+    private int numeric;
+
+    public int Numeric
+    {
+        set
+        {
+            numeric = value;
+            textBlockNumber.text = numeric.ToString();
+            blockImage.color = blockColorList[(int)Mathf.Log(value, 2) - 1];
+        }
+        get => numeric;
+    }
+
+    public void Initialized()
+    {
+        Numeric = Random.Range(0, 100) < 90 ? 2 : 4;
+    }
 }
