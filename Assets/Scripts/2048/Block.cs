6124558bf536dfe93babcd2bf19b5ea63bb72a45
Minwoo Kim <minwook1130@gmail.com>
2023-12-18 17:01:56 -0500

    Added Block Spawning Animation

--- Assets/Scripts/2048/Block.cs
+++ Assets/Scripts/2048/Block.cs
@@ -29,5 +29,23 @@ public class Block : MonoBehaviour
     public void Initialized()
     {
         Numeric = Random.Range(0, 100) < 90 ? 2 : 4;
+
+        StartCoroutine(OnScaleTo(Vector3.zero, Vector3.one, 0.15f));
+    }
+
+    private IEnumerator OnScaleTo(Vector3 start, Vector3 end, float time)
+    {
+        float current = 0;
+        float percent = 0;
+
+        while ( percent < 1 )
+        {
+            current += Time.deltaTime;
+            percent = current / time;
+
+            transform.localScale = Vector3.Lerp(start, end, percent);
+
+            yield return null;
+        }
     }
 }
