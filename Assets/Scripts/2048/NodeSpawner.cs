d840f90553635a249034c7dc22d32b1c37b08c0e
Minwoo Kim <minwook1130@gmail.com>
2023-12-19 15:36:50 -0500

    Updated Node Spawner
    Node has a neighbour Nodes information itself

--- Assets/Scripts/2048/NodeSpawner.cs
+++ Assets/Scripts/2048/NodeSpawner.cs
@@ -23,14 +23,24 @@ public class NodeSpawner : MonoBehaviour
                 clone.name = $"Node[ {x}, {y} ]";
                 // Fetch Node component and initialize it
                 Node node = clone.GetComponent<Node>();
+                // Get the Coordinate value 
                 Vector2Int coordinate = new Vector2Int(x, y);
 
+                // Create a nullable array for NeighbourNodes of this Node
                 Vector2Int?[] neighbourNodes = new Vector2Int?[4];
-
+                // Get the 4 Nodes in 4 different directions
                 Vector2Int up = coordinate + Vector2Int.down;
                 Vector2Int right = coordinate + Vector2Int.right;
                 Vector2Int down = coordinate + Vector2Int.up;
                 Vector2Int left = coordinate + Vector2Int.left;
+                // Check if the neighbour Nodes are within the board
+                if (IsValidNode(up, blockCount)) neighbourNodes[0] = up;
+                if (IsValidNode(right, blockCount)) neighbourNodes[1] = right;
+                if (IsValidNode(down, blockCount)) neighbourNodes[2] = down;
+                if (IsValidNode(left, blockCount)) neighbourNodes[3] = left;
+
+                // Initialize the Node upon spawning
+                node.Initialized(board, neighbourNodes, coordinate);
 
                 // Add the Node to the List
                 nodeList.Add(node);
@@ -38,5 +48,15 @@ public class NodeSpawner : MonoBehaviour
         }
         // return the List of Nodes
         return nodeList;
+    }
+
+    private bool IsValidNode(Vector2Int position, Vector2Int blockCount)
+    {
+        if (position.x == -1 || position.x == blockCount.x || position.y == -1 || position.y == blockCount.y)
+        {
+            return false;
+        }
+
+        return true;
     }
 }
