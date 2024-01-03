42ebf52a23fcf451d9e955f5874dc884e0f43ed7
Minwoo Kim <minwook1130@gmail.com>
2024-01-03 15:56:30 -0500

    Block script in progress

--- Assets/Scripts/2048/Block.cs
+++ Assets/Scripts/2048/Block.cs
@@ -1,8 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField]
+    private Color[] blockColorList;
+    [SerializeField]
+    private Image block_image;
+    [SerializeField]
+    private TextMeshProUGUI blockNumeric_text;
 
+    private int numeric;
+
+    public int Numeric
+    {
+        set
+        {
+            Numeric = value;
+
+            blockNumeric_text.text = Numeric.ToString();
+        }
+
+        get => numeric;
+    }
 }
