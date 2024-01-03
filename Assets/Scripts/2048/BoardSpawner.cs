e913d4d864f41cd239432a295951600859a5a4a7
Minwoo Kim <minwook1130@gmail.com>
2024-01-03 18:14:24 -0500

    Updated Block script
    - Added NeighbourSlots to each Block object

--- Assets/Scripts/2048/BoardSpawner.cs
+++ Assets/Scripts/2048/BoardSpawner.cs
@@ -30,10 +30,15 @@ public class BoardSpawner : MonoBehaviour
                 Vector2Int coordinate = new Vector2Int(x, y);
 
                 Vector2Int?[] neighbourSlots = new Vector2Int?[4];
-                neighbourSlots[0] = coordinate + new Vector2Int(0, -1); // or Vector2Int.down
-                neighbourSlots[1] = coordinate + Vector2Int.right;
-                neighbourSlots[2] = coordinate + new Vector2Int(0, 1);  // or Vector2Int.up
-                neighbourSlots[3] = coordinate + Vector2Int.left;
+                Vector2Int up = coordinate + new Vector2Int(0, -1); // or Vector2Int.down
+                Vector2Int right = coordinate + Vector2Int.right;
+                Vector2Int down = coordinate + new Vector2Int(0, 1);  // or Vector2Int.up
+                Vector2Int left = coordinate + Vector2Int.left;
+
+                if (IsValid(boardCount, up)) neighbourSlots[0] = up;
+                if (IsValid(boardCount, right)) neighbourSlots[1] = right;
+                if (IsValid(boardCount, down)) neighbourSlots[2] = down;
+                if (IsValid(boardCount, left)) neighbourSlots[3] = left;
 
                 slot.Initialized(boardManager, neighbourSlots, coordinate);
 
@@ -42,5 +47,16 @@ public class BoardSpawner : MonoBehaviour
         }
 
         return board;
+    }
+
+    private bool IsValid(Vector2Int boardCount, Vector2Int slotCoordinate)
+    {
+        if ( slotCoordinate.x > boardCount.x || slotCoordinate.y > boardCount.y ||
+             slotCoordinate.x < 0 || slotCoordinate.y < 0 )
+        {
+            return false;
+        }
+
+        return true;
     }
 }
