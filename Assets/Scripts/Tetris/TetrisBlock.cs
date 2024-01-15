407efcc4c141b58bff1ccfa2af2494bb19325233
Minwoo Kim <minwook1130@gmail.com>
2024-01-15 16:00:58 -0500

    Adding comments on each line on code in progress
    - To track back how all of codes work

--- Assets/Scripts/Tetris/TetrisBlock.cs
+++ Assets/Scripts/Tetris/TetrisBlock.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class TetrisBlock : MonoBehaviour
 {
+    // The Controller when the TetrisBlock is active in the field
     private TetrisBlockController controller;
+    // Time/Speed of the TetrisBlock going down
     private float downFrame = 2f;
+    // A timer to measure time. Every [downFrame] time, the TetrisBlock goes 1 row lower
     private float timer = 0;
 
+    // TetrisBlock's Color
     public Color Color { get; private set; }
 
     private void Update()
@@ -26,15 +30,21 @@ public class TetrisBlock : MonoBehaviour
 
     }
 
+    // When TetrisBlock is spawned on the field
     public void OnBoard()
     {
+        // Initialize the Controller
         controller = new TetrisBlockController();
+        // Register the controlling method
         controller.inputController += UponMoving;
     }
 
+    // When TetrisBlock has landed on the field
     public void OffBoard()
     {
+        // Deregister the controlling method
         controller.inputController -= UponMoving;
+        // Delete the Controller object
         controller = null;
     }
 
